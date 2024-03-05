03ff9f64729fda905ef89e23c45cacbc79c39546
Patience-Sibanda <psa.khanye@gmail.com>
2024-03-05 09:38:29 -0800

    All

--- print_array.c
+++ print_array.c
@@ -1,23 +1,23 @@
-#include <stdlib.h>
+#include "sort.h"
 #include <stdio.h>
 
 /**
- * print_array - prints an array of integers
- *
- * @array: The array to be printed
- * @size : Number of the element in @array
+ * print_array - Prints an array of integers
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
  */
 void print_array(const int *array, size_t size)
 {
 	size_t i;
 
-	i = 0;
-	while (array && i < size)
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size; i++)
 	{
-		if (i > 0)
-			printf(", ");
 		printf("%d", array[i]);
-		++i;
+		if (i != size - 1)
+			printf(", ");
 	}
 	printf("\n");
 }
